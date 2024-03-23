776cbfbee0f898328c8cfcd217f3d727e13a176e
capuchinojt <36061313+capuchinojt@users.noreply.github.com>
2024-03-23 10:14:47 +0700

    Using redux-persist to store redux state into local storage
    Using redux-persist to store redux state into local storage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import App from '@/App.jsx'
-import { store } from '@/services/redux/store'
+import App from '@/App'
+import { store, persistor } from '@/services/redux/store'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>,
 )
--- client/src/services/redux/store.jsx
+++ client/src/services/redux/store.jsx
@@ -1,15 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import signUpReducer from '@/services/redux/signUp/signUpSlice'
 import signInReducer from '@/services/redux/signIn/signInSlice'
+import persistStore from 'redux-persist/es/persistStore'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1
+}
+
+const rootReducer = combineReducers({
+  signUp: signUpReducer,
+  signIn: signInReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    signUp: signUpReducer,
-    signIn: signInReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     })
 })
+
+export const persistor = persistStore(store)
