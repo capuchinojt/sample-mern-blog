e191799873868257e9df5aee454fe46d5fa43e6e
capuchinojt <36061313+capuchinojt@users.noreply.github.com>
2024-04-02 23:26:50 +0700

    Fix warning problem about [Cognitive Complexity] - Warning message: Refactor this function to reduce its Cognitive Complexity from 22 to the 15 allowed. [+15 locations]

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,61 +8,80 @@ export const getUserInfo = (req, res) => {
   res.json({ message: 'Welcome to user route'})
 }
 
-export const updateUserInfo = async (req, res, next) => {
-  console.log('updateUserInfo', req.user)
-  if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to update.'))
-  }
+const checkAndUpdatePassword = async (req) => {
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, 'Password must be at least 6 characters.'))
+      throw new Error('Password must be at least 6 characters.')
     }
-    req.body.password = await bcryptjs.hash(req.body.password, 10)
+    return await bcryptjs.hash(req.body.password, 10)
   }
+  return null
+}
+
+const checkAndUpdateUsername = async (req) => {
   if (req.body.username) {
     const username = req.body.username.toLowerCase()
     if (username.length < 7 || username.length > 20) {
-      return next(errorHandler(400, 'Username must be between 7 and 20 characters.'))
+      throw new Error('Username must be between 7 and 20 characters.')
     }
     if (username.includes(" ")) {
-      return next(errorHandler(400, 'Username can not contain spaces.'))
+      throw new Error('Username can not contain spaces.')
     }
     if (!username.match(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/)) {
-      return next(errorHandler(400, 'Username must contain both letters and numbers.'));
+      throw new Error('Username must contain both letters and numbers.')
     }
 
     const existingUser = await User.findOne({ username });
     if (existingUser && existingUser._id.toString() !== req.params.userId) {
-        return next(errorHandler(400, 'Username is already taken.'));
+      throw new Error('Username is already taken.')
     }
 
-    req.body.username = username; // Cập nhật username đã được xử lý
+    return username
   }
+  return null
+}
+
+const checkEmail = async (req) => {
   if (req.body.email) {
     const email = req.body.email
     if (!validateEmail(email)) {
-      return next(errorHandler(400, 'Email is not valid.'))
+      throw new Error('Email is not valid.')
     }
 
     const existingUserByEmail = await User.findOne({ email }).select('_id')
     console.log('existingUserByEmail: ', existingUserByEmail)
     if (existingUserByEmail && existingUserByEmail._id.toString() !== req.params.userId) {
-      return next(errorHandler(400, 'Email is already in use.'))
+      throw new Error('Email is already in use.')
     }
-  }
 
+    return email
+  }
+  return null
+}
+
+export const updateUserInfo = async (req, res, next) => {
   try {
+    console.log('updateUserInfo', req.user)
+    if (req.user.id !== req.params.userId) {
+      return next(errorHandler(403, 'You are not allowed to update.'))
+    }
+    const password = await checkAndUpdatePassword(req)
+    const username = await checkAndUpdateUsername(req)
+    const email = await checkEmail(req)
+
+    const updateFields = {
+      ...(password && { password }),
+      ...(username && { username }),
+      ...(email && { email }),
+      ...req.body.profilePicture && { profilePicture: req.body.profilePicture }
+    }
+
     const updatedUser = await User.findByIdAndUpdate(req.params.userId, {
-      $set: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        profilePicture: req.body.profilePicture
-      }
+      $set: updateFields
     }, { new: true }).select('-password')
     res.status(200).json(updatedUser)
   } catch (error) {
     console.error('Error updating user info:', error)
-    next(error)
+    next(errorHandler(400, error.message))
   }
 }
