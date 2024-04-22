08cad4acbf6431f84d0a5d22943b7cf25fae1570
NDHuy <ndhuy.work@gmail.com>
2024-04-22 20:58:10 +0700

    Complete upload file in create post page
    Complete upload file in create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,25 +1,35 @@
+import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
-import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
+import { Alert, Button, FileInput, Select, Spinner, TextInput } from 'flowbite-react'
+import { CircularProgressbar } from "react-circular-progressbar"
+import 'react-circular-progressbar/dist/styles.css'
 import { useMutation } from '@tanstack/react-query'
 import ReactQuill from "react-quill"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
 import 'react-quill/dist/quill.snow.css'
 import * as yup from 'yup'
 import _ from 'lodash'
-
 import { yupResolver } from "@hookform/resolvers/yup"
-import { useEffect, useState } from "react";
+
 import { createPostRequest } from "@/api/postApi"
+import { app } from "@/firebase"
+import { messageType } from "@/constant/status.constants"
 
 export const CreatePost = () => {
+  const [file, setFile] = useState(null)
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null)
+  const [imageFileUploading, setImageFileUploading] = useState(null)
+  const [imageUploadError, setImageUploadError] = useState(null)
   const postSchema = yup.object({
     title: yup.string().required()
   })
-  const {register, handleSubmit, setValue, watch, formState: {errors}} = useForm({
+  const {register, handleSubmit, setValue, getValues, watch, formState: {errors}} = useForm({
     resolver: yupResolver(postSchema)
   })
 
   useEffect(() => {
-    register('editorContent', { required: true })
+    register('editorContent', { required: true }),
+    register('image')
   }, [register])
 
   const [notificationContent, setNotificationContent] = useState(null)
@@ -27,10 +37,16 @@ export const CreatePost = () => {
   const createPostMutation = useMutation({
     mutationFn: (postData) => createPostRequest(postData),
     onSuccess: (response) => {
-      setNotificationContent(response?.data?.message)
+      setNotificationContent({
+        type: messageType.success,
+        message: response?.data?.message
+      })
     },
     onError: (response) => {
-      setNotificationContent(response?.data?.message)
+      setNotificationContent({
+        type: messageType.failure,
+        message: response?.data?.message
+      })
     }
   })
 
@@ -42,12 +58,53 @@ export const CreatePost = () => {
     const postData = {
       title: data.title,
       category: data.category,
-      content: data.editorContent
+      content: data.editorContent,
+      image: getValues('image')
     }
     createPostMutation.mutate(postData)
   }
 
   const editorContent = watch('editorContent')
+
+  const handleuUploadImage = () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select your image file.')
+        return
+      }
+      const storage = getStorage(app)
+      const fileName = new Date().getTime() + '-' + file.name
+      const storageRef = ref(storage, fileName)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          setImageFileUploading(true)
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          setImageFileUploadProgress(progress.toFixed(0))
+        },
+        // When upload failed
+        (error) => {
+          setImageUploadError("Image upload failed. Image's size must be less than 2MB")
+          setImageFileUploadProgress(null)
+          setFile(null)
+          setImageFileUploading(false)
+          console.log('Image upload failed:: ', error)
+        },
+        () => {
+          // When upload succeeded
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageFileUploadProgress(null)
+            setImageUploadError(null)
+            setValue('image', downloadURL)
+          })
+        }
+      )
+    } catch (error) {
+      setImageUploadError('Image upload failed')
+      setImageFileUploadProgress(null)
+      console.log('Image upload failed:: ', error)
+    }
+  }
 
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
@@ -63,15 +120,37 @@ export const CreatePost = () => {
           </Select>
         </div>
         <div className="flex gap-4 border-4 items-center justify-between border-teal-500 border-dotted p-3">
-          <FileInput type="file" accept="image/*"/>
-          <Button type="button" gradientDuoTone='purpleToBlue' size='sm'>Upload File</Button>
+          <FileInput type="file" accept="image/*" onChange={(e) => setFile(e.target.value[0])} />
+          {
+            imageUploadError && (
+              <Alert color='failure' onDismiss={() => alert('Alert dismissed!')}>
+                <span className="font-medium">Error!</span> {imageUploadError}
+              </Alert>
+            )
+          }
+          <Button type="button" disabled={imageFileUploadProgress || imageFileUploading} gradientDuoTone='purpleToBlue' size='sm' onClick={handleuUploadImage}>
+            {
+              imageFileUploadProgress ? (
+                <div className="w-16 h-16">
+                  <CircularProgressbar value={imageFileUploadProgress} text={`${imageFileUploadProgress || 0}%`} />
+                </div>
+              ) : 'Upload File'
+            }
+          </Button>
         </div>
         <ReactQuill theme="snow" value={editorContent} placeholder="Write something..." className="h-72 mb-12" onChange={onContentChange} />
-        <Button type="submit" gradientDuoTone="purpleToPink">Publish</Button>
+        <Button type="submit" gradientDuoTone="purpleToPink" disabled={createPostMutation.isPending}>
+          { 
+            createPostMutation.isPending || imageFileUploading ? <>
+              <Spinner size='sm' />
+                <span className="pl-3">Loading...</span>
+            </> : 'Publish'
+          }
+        </Button>
         {
           notificationContent && (
-            <Alert color="success" onDismiss={() => alert('Alert dismissed!')}>
-              <span className="font-medium">Successfull!</span> {notificationContent}
+            <Alert color={notificationContent.type} onDismiss={() => alert('Alert dismissed!')}>
+              <span className="font-medium">Successfull!</span> {notificationContent.message}
             </Alert>
           )
         }
