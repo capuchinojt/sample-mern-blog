b4d0e21da8317c5874c58081fff99a99d2600b99
capuchinojt <36061313+capuchinojt@users.noreply.github.com>
2024-04-30 12:31:36 +0700

    refactor and add more condition to get posts
    refactor and add more condition to get posts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,6 @@
+import { query } from "express"
 import { ERROR_CODES } from "../constant/errorStatus.constant.js"
+import Post from "../models/post.model.js"
 import PostModel from "../models/post.model.js"
 import { errorHandler } from "../utils/error.handle.js"
 
@@ -37,10 +39,53 @@ export const createPost = async (req, res, next) => {
 
 export const getPosts = async (req, res, next) => {
   try {
-    const posts = await PostModel.find()
-    res.status(200).json(posts)
+    const startIndex = parseInt(req.query.startIndex) || 0
+    const limit = parseInt(req.query.limit) || 9
+    const sortDirection = req.query.order === 'asc' ? 1 : -1
+    const conditions = buildQueryCondition(req.query)
+    const posts = await PostModel.find(conditions)
+                                 .sort({ updatedAt: sortDirection })
+                                 .skip(startIndex)
+                                 .limit(limit)
+    const postCount = posts.length
+    
+    const totalPosts = await PostModel.countDocuments()
+
+    const now = new Date()
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+
+    const lastMonthPosts = await PostModel.countDocuments({
+      createdAt: { $gte: oneMonthAgo }
+    })
+
+    res.status(200).json({
+      posts,
+      postCount,
+      totalPosts,
+      lastMonthPosts
+    })
   } catch (error) {
     console.error('Error get post::', error.message)
     next(errorHandler(ERROR_CODES.BAD_REQUEST, 'An error occurred while get posts.'))
   }
+}
+
+const buildQueryCondition = (query) => {
+  let conditions = {};
+  if (query?.userId) conditions.userId = query.userId;
+  if (query?.category) conditions.category = query.category;
+  if (query?.slug) conditions.slug = query.slug;
+  if (query?.postId) conditions._id = query.postId;
+  if (query?.searchTerm) {
+      conditions.$or = [
+          { title: { $regex: query.searchTerm, $options: 'i' } },
+          { content: { $regex: query.searchTerm, $options: 'i' } }
+      ];
+  }
+  return conditions;
 }
