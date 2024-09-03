625ca65e46c2a106f4a0ba878db97dc7447585a2
capuchinojt <36061313+capuchinojt@users.noreply.github.com>
2024-09-03 15:36:26 +0700

    Add comment to OAuth function using firebase, google auth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router-dom'
 import { Button } from "flowbite-react"
 import { AiFillGoogleCircle } from "react-icons/ai"
+// Import Firebase authentication functions
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 
+// Import the Firebase app instance
 import { app } from "@/firebase"
 import { useMutation } from "@tanstack/react-query"
 import { signInWithGoogleRequest } from "@/api/authApi"
@@ -21,18 +23,24 @@ export const OAuth = () => {
   })
 
   const handleGoogleAuthClick = async () => {
+    // Initialize Firebase Auth instance
     const auth = getAuth(app)
+    // Create a new Google Auth Provider
     const provider = new GoogleAuthProvider()
+    // Set custom parameters for the Google sign-in prompt
     provider.setCustomParameters({ prompt: 'select_account' })
 
     try {
+      // Trigger Google sign-in popup and await the result
       const resultFromGoogleAuth = await signInWithPopup(auth, provider)
       if (resultFromGoogleAuth?._tokenResponse) {
+        // Extract user information from the Google Auth result
         const userInfo = {
           name: resultFromGoogleAuth.user.displayName,
           email: resultFromGoogleAuth.user.email,
           googlePhotoUrl: resultFromGoogleAuth.user.photoURL
         }
+        // Trigger the mutation to sign in with Google on the backend
         mutationSignInWithGoogle.mutate(userInfo)
       }
       console.info('resultFromGoogleAuth', resultFromGoogleAuth)
